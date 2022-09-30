299235df9409fac232c9adb72174430fae58f75a
obelisk02 <70256661+obelisk02@users.noreply.github.com>
2022-09-30 13:39:24 -0500

    "final v1""
    none

--- config/index.js
+++ config/index.js
@@ -1,11 +1,17 @@
 require('dotenv').config();
 
+const MONGO_USERNAME = process.env.MONGO_USERNAME || "";
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD || "";
+const MONGO_HOST = process.env.MONGO_HOST || "";
+
 const config = {
     port: process.env.PORT || 3000,
     db_host: process.env.DB_HOST || 'localhost',
     db_port: process.env.DB_PORT || 27017,
     db_name: process.env.DB_NAME || 'sciocourses',
-    secret: process.env.SECRET || 'secret'
+    secret: process.env.SECRET || 'secret',
+
+    url : `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}`
 };
 
 
--- utils/database.js
+++ utils/database.js
@@ -2,6 +2,6 @@ const mongo = require('mongoose');
 
 const config = require("../config");
 
-const mongoDBConnection = mongo.connect(`mongodb://${config.db_host}:${config.db_port}/${config.db_name}`);
+const mongoDBConnection = mongo.connect(config.url);
 
 module.exports = mongoDBConnection;
